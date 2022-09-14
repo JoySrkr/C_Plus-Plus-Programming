6636924b7ce0864ae5a00a1fa1a28e7a29026c04
JoySrkr <105807836+JoySrkr@users.noreply.github.com>
2022-09-14 21:35:12 +0600

    Add files via upload

---
+++ C++ Practice/MyFirstProgram.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+   string fullName;
+   cout<< "Type your Full Name: ";
+   getline(cin,fullName);
+   cout<< "Your Name is: "<<fullName;
+   return 0;
+}
